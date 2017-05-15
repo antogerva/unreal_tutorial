a2d1b2b2b8fe0ae3e5dc3098f30e68c036599a44
antogerva <antogerva@gmail.com>
2017-05-14 22:28:36 -0400

    Added PositionReport component

---
+++ Source/EscapeBld/EscapeBld.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EscapeBld.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeBld, "EscapeBld" );
